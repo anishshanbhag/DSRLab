811920ecf4c04d6e5848ef3726bdf7ac5a58c7d7
Anish <anish@Anishs-MacBook-Pro.local>
2019-11-20 21:42:35 +0530

    init

--- 1st Lab/firstLab-matrix.R
+++
@@ -1,6 +0,0 @@
-mat1 = matrix(data=c(12,14,15,16),nrow = 2, ncol=2)
-mat1
-mat2 = matrix(data=c(10,11,12,14),nrow = 2, ncol=2)
-mat2
-mat3 = mat1%*% mat2
-mat3
--- 1st Lab/firstLab-vector.R
+++
@@ -1,8 +0,0 @@
-vec<-c(10,20,30,40,50)
-vec
-vec*2
-vec/2
-vec%%2 #asfsfasf
-vec + 6
-vex<- c('one','two','three','four','five')
-vex
